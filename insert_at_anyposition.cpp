4280c6935764ed82b21da90a58b7b976c0ec9a40
rizwanhossainrahat <rizwanhossainrahat400@gmail.com>
2023-09-09 12:49:39 +0600

    doubly linklist operation

---
+++ insert_at_anyposition.cpp
@@ -0,0 +1,130 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void print_normal(Node* head)
+{
+    Node* tmp=head;
+    while(tmp!=NULL)
+    {
+        cout<<tmp->val<<" ";
+        tmp=tmp->next;
+    }
+    cout<<endl;
+}
+void print_reverse(Node* tail)
+{
+    Node* tmp=tail;
+    while(tmp!=NULL)
+    {
+        cout<<tmp->val<<" ";
+        tmp=tmp->prev;
+    }
+
+}
+void insert_at_position(Node* head,int pos,int val)
+{
+  Node* newNode = new Node(val);
+  Node* tmp=head;
+  for(int i=1;i<=pos-1;i++)
+  {
+    tmp=tmp->next;
+  }
+ newNode->next=tmp->next;
+ tmp->next=newNode;
+ newNode->next->prev=newNode; //100->30
+ newNode->prev=tmp; //20->100
+}
+
+void insert_at_head(Node* &head,Node* &tail,int val)
+{
+  Node* newnode = new Node(val);
+    Node* tmp=head;
+    if(head==NULL)
+    {
+        head=newnode;
+        tail=newnode;
+        return;
+    }
+    newnode->next=head;
+    head->prev=newnode;
+    head=newnode;
+}
+void insert_at_tail(Node* &head,Node* &tail,int val)
+{
+    Node* newnode=new Node(val);
+    if(tail==NULL)
+    {
+        head=newnode;
+        tail=newnode;
+        return;
+    }
+    tail->next=newnode;
+    newnode->prev=tail;
+    tail=tail->next;
+    // tail=newnode;
+}
+int size(Node* head)
+{
+    Node* tmp=head;
+    int cnt=0;
+    while (tmp!=NULL)
+    {
+       cnt++;
+       tmp=tmp->next;
+    }
+    return cnt;
+}
+
+int main()
+{
+    // Node* head=NULL;
+    // Node* tail=NULL;
+    Node* head=new Node(10);   
+    Node* a=new Node(20);   
+    Node* b=new Node(30); 
+    Node* c=new Node(40); 
+    Node* tail=c;  
+
+    head->next=a;
+    a->prev=head;
+    a->next=b;
+    b->prev=a;
+    b->next=c;
+    c->prev=b;
+    int pos,val;
+    cin>>pos>>val;
+
+    if(pos>size(head))
+    {
+        cout<<"Invalid"<<endl;
+    }
+    else if(pos==0)
+    {
+        insert_at_head(head,tail,val);
+    }
+    else if(pos==size(head))
+    {
+        insert_at_tail(head,tail,val);
+    }
+    else
+    {
+         insert_at_position(head,pos,100);
+    }
+   
+
+    print_normal(head);
+    print_reverse(tail);
+    return 0;
+}
